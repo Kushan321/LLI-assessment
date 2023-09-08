2b9fddfd3b17f74277ee942425d20216870ff67d
Kushan Kekirideniya <kushanravindu@arimaclanka.com>
2023-09-08 21:16:07 +0530

    min change

--- src/components/ui-components/addVehicles.vue
+++ src/components/ui-components/addVehicles.vue
@@ -535,7 +535,7 @@ export default {
 
 <style lang="scss">
 .vehicles-adding-wrapper {
-  padding: 0px 20px;
+  padding: 0px 20px 20px 0px;
   margin-bottom: 50px;
   .QRcode-section {
     margin-top: 20px;
